ca7946394d9b28f8641f89d38183cb8286ba0888
小盛开同学 <354830981@qq.com>
2020-12-11 00:52:46 +0800

    Update queueWatcher.js

--- src/queueWatcher.js
+++ src/queueWatcher.js
@@ -1,6 +1,4 @@
-/** 
- * 过滤重复watcher
-*/
+
 import nextTick from './next-tick'
 
 let has = {}       // watcher.id map 唯一值
@@ -8,17 +6,22 @@ let queue = []     // watcher map 唯一值
 let waiting = false // 标记位
 
 export default function queueWatcher (watcher) {
+  /** 
+   * 过滤重复watcher
+  */
   const id = watcher.id
   if (!has[id]) {
     has[id] = true
     queue.push(watcher)
     if (!waiting) {
       waiting = true
+      // 上述操作完成后，走异步解析
       nextTick(flushSchedulerQueue)
     }
   }
 }
 
+// 异步完之后，数据整理好了（这里简单去重），然后去跑run渲染
 function flushSchedulerQueue () {
   let watcher, id
   for (let i = 0; i < queue.length; i++) {
